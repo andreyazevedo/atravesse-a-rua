d52e2350768b6bfcfe24cb1a48952193e7e487c6
Andrey Azevedo <andreyaze@gmail.com>
2017-08-20 01:50:07 +0000

    enemies moves

--- js/app.js
+++ js/app.js
@@ -6,19 +6,43 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -101;
+    this.y = this.randomPosition();
+    this.speed = this.randomSpeed();
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.walk(dt);
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Seleciona aleatoriamente um numero entre as posições
+// que os inimigos podem aparecer inicialmente no mapa
+Enemy.prototype.randomPosition = function() {
+  var positions = [60, 140, 220];
+  return positions[Math.floor((Math.random()*positions.length))];
+};
+
+Enemy.prototype.randomSpeed = function () {
+  var speeds = [200, 220, 280];
+  return speeds[Math.floor((Math.random()*speeds.length))];
+};
+
+Enemy.prototype.walk = function (dt) {
+  var speed = this.speed * dt;
+
+  if (this.x + speed <= 504) {
+      this.x = this.x + speed;
+  } else {
+    this.x = -101;
+    this.y = this.randomPosition();
+  }
 };
 
 // Now write your own player class
@@ -85,7 +109,10 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-allEnemies.push(new Enemy);
+
+for (var i = 0; i < 4; i++) {
+  allEnemies.push(new Enemy);
+}
 
 var player = new Player;
 
